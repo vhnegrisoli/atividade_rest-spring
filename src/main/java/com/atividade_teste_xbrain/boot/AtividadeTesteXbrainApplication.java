e45c7339b6ff78527861340c994b1de557c99749
vhnegrisoli <victorhugonegrisoli.ccs@hotmail.com>
2018-11-30 07:27:03 -0200

    Projeto inicializado com Spring Boot utilizando Spring Initializr

---
+++ src/main/java/com/atividade_teste_xbrain/boot/AtividadeTesteXbrainApplication.java
@@ -0,0 +1,12 @@
+package com.atividade_teste_xbrain.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeTesteXbrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeTesteXbrainApplication.class, args);
+	}
+}
