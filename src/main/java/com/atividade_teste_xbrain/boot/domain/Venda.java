de86c96c99d934ce8f2719d8044c70a07acb3710
vhnegrisoli <victorhugonegrisoli.ccs@hotmail.com>
2018-11-30 22:59:31 -0200

    Foi criada a Entity Venda, fazendo uma ligação N para 1 com Vendedor para retornar os dados da API.

---
+++ src/main/java/com/atividade_teste_xbrain/boot/domain/Venda.java
@@ -0,0 +1,94 @@
+package com.atividade_teste_xbrain.boot.domain;
+
+/*
+ * Esta classe de entidade é responsável por mapear a tabela Venda no banco de dados H2, sendo
+ * mapeada pelo framework Hibernate através da especificação JPA adicionada à injeção de dependências
+ * do projeto.
+ * 
+ * @author Victor Hugo Negrisoli
+ * @version 1.0
+ * 
+ */
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "venda")
+
+public class Venda implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_venda")
+    private Integer idVenda;
+    
+    @Column(name = "data_venda")
+    private String dataVenda;
+    
+    @JoinColumn(name = "id_vendedor", referencedColumnName = "id_vendedor")
+    @ManyToOne(optional = false)
+    private Vendedor idVendedor;
+
+    public Venda() {
+    }
+
+    public Venda(Integer idVenda) {
+        this.idVenda = idVenda;
+    }
+
+    public Integer getIdVenda() {
+        return idVenda;
+    }
+
+    public void setIdVenda(Integer idVenda) {
+        this.idVenda = idVenda;
+    }
+
+    public String getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(String dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+    public Vendedor getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(Vendedor idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idVenda != null ? idVenda.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Venda)) {
+            return false;
+        }
+        Venda other = (Venda) object;
+        if ((this.idVenda == null && other.idVenda != null) || (this.idVenda != null && !this.idVenda.equals(other.idVenda))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "testetabelas.Venda[ idVenda=" + idVenda + " ]";
+    }
+    
+}
