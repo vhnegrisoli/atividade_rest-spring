1fb67c2b6a26ccc276600cff54c22d4d46f78887
vhnegrisoli <victorhugonegrisoli.ccs@hotmail.com>
2018-12-01 10:42:26 -0200

    Criado o pacote services e a classe VendedorService, responsável por criar o serviço entre as classes Repository e o Controller da API Rest

---
+++ src/main/java/com/atividade_teste_xbrain/boot/service/VendedorService.java
@@ -0,0 +1,39 @@
+package com.atividade_teste_xbrain.boot.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/*
+ * A classe VendedorService tem como objetivo oferecer um serviço de acesso aos objetos de banco 
+ * de dados das interfaces Repository que são especificadas e mapeadas pela JPA.
+ * 
+ * @author Victor Hugo Negrisoli
+ * @version 1.0
+ * 
+ */
+
+import org.springframework.stereotype.Service;
+
+import com.atividade_teste_xbrain.boot.domain.Vendedor;
+import com.atividade_teste_xbrain.boot.repository.VendedorRepository;
+
+@Service
+public class VendedorService {
+	
+	@Autowired
+	private VendedorRepository vendedorRepository;
+	
+	/*
+	 * O método buscar é responsável por receber um parâmetro idVendedor
+	 * e retornar o objeto Vendedor responsável no banco de dados através da instanciação de
+	 * vendedorRepository.
+	 * 
+	 * @Return List vendedor Lista de objetos do tipo Vendedor contendo todos os objetos de Vendedor
+	 * que estão no banco de dados
+	 */
+	public List<Vendedor> buscarTodos() {
+		return vendedorRepository.findAll();
+	}
+	
+}
